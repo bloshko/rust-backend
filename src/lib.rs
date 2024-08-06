6c3cb315cbcba07f572334a7bf700abbe9d77bff
bloshko <mis.bloko@gmail.com>
2024-08-06 17:31:41 +0200

    Add rustfmt.toml config

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn greet() -> impl Responder {
     "text"
@@ -13,8 +13,12 @@ async fn health_check() -> impl Responder {
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-        App::new().route("/", web::get().to(greet)).route("/health_check", web::get().to(health_check))
-    }).listen(listener)?.run();
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,16 @@
 use std::net::TcpListener;
 
-
 #[tokio::test]
 async fn health_check_works() {
-
     let address = spawn_app();
 
     let client = reqwest::Client::new();
 
-    let response = client.get(format!("{}/health_check", address)).send().await.expect("Failed to execute request");
+    let response = client
+        .get(format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute request");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
